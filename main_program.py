7eac5e9662914f913faf52502406a7d35bb650e4
GraydonHall42 <gwhall@ualberta.ca>
2021-06-14 21:42:14 -0600

    added further commenting

--- main_program.py
+++ main_program.py
@@ -6,8 +6,7 @@
 from individual_pos_stats import ind_player_stats
 from genereal_stats import run_general_stats
 from player_comparison import player_compare
-ind_player_stats()
-run_general_stats()
-player_compare("QB")
-player_compare("RB")
-# stats for all positions
+ind_player_stats()  # terminal program that gets user input, gives data specific to speicified position
+run_general_stats()  # terminal application that shows tables for all positions, and prints plots comparint the positions
+player_compare("QB")  # gives graph comparing how QBs got their points (passing, rushing, other)
+player_compare("RB")  # gives graph comparing how RBs got their points (receiving, rushing, other)
--- player_comparison.py
+++ player_comparison.py
@@ -13,19 +13,24 @@ pd.set_option('display.max_columns', 17)
 
 
 def player_compare(pos):
+    """
+    function which takes a position as String, and returns a graph showing how different areas of their game
+    contribured to their point totals
+    :param pos: String position to investigate (ex QB, WR, TE)
+    """
     if pos == "QB":
-        optional_col = "Passing Pts"
+        optional_col = "Passing Pts"  # want passing if it's a QB
     else:
-        optional_col = "Receiving Pts"
+        optional_col = "Receiving Pts"  # otherwise want receiving
 
     idx = pd.IndexSlice
-    req_cols = ['NAME', 'TTL PTS', 'Rushing Pts', optional_col]
-    position_df = final_df[req_cols].loc[idx['PPR', pos], :].copy()
-    position_df.dropna(inplace=True)
-    position_df['Other Pts'] = position_df['TTL PTS'] - position_df['Rushing Pts'] - position_df[optional_col]
-    position_df[f'{pos} Rank'] = position_df['TTL PTS'].rank(ascending=False)
-    position_df.sort_values(by='TTL PTS', inplace=True, ascending=False)
-    position_df.head(20).plot.bar(x=f'{pos} Rank', y=['Rushing Pts', optional_col, 'Other Pts'], stacked=True)
+    req_cols = ['NAME', 'TTL PTS', 'Rushing Pts', optional_col]  # columns to study
+    position_df = final_df[req_cols].loc[idx['PPR', pos], :].copy()  # copy of our main DF filterted to ppr and position
+    position_df.dropna(inplace=True)  # drop null rows
+    position_df['Other Pts'] = position_df['TTL PTS'] - position_df['Rushing Pts'] - position_df[optional_col]  # Calculated column for other points that aren't rushing or passing/receiving
+    position_df[f'{pos} Rank'] = position_df['TTL PTS'].rank(ascending=False)  # ranking column based on ttl pts
+    position_df.sort_values(by='TTL PTS', inplace=True, ascending=False)  # sort values by ranking
+    position_df.head(20).plot.bar(x=f'{pos} Rank', y=['Rushing Pts', optional_col, 'Other Pts'], stacked=True)  # plot data
     plt.title(f"Fantasy {pos} point breakdown")
     plt.ylabel("Fantasy Points")
     plt.show()
